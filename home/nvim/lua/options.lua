24cb1ac344fbafb731fb9204b0cc6e5b2e83e637
Drew Schrauf <drew@qwilr.com>
2026-02-12 07:02:50 +0000

    Refresh buffer on focus

--- home/nvim/lua/options.lua
+++ home/nvim/lua/options.lua
@@ -3,3 +3,9 @@ require "nvchad.options"
 vim.o.colorcolumn = "100"
 vim.opt.tabstop = 2
 vim.opt.swapfile = false
+vim.o.autoread = true
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  pattern = "*",
+  command = "checktime",
+})
